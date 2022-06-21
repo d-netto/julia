d6350dbd2836f07c0a762ce4f1ee6accda5bd6fe
Diogo Netto <dcn@Diogos-MacBook-Air.local>
2022-06-21 11:10:27 -0400

    minor formatting fix

--- src/gc.c
+++ src/gc.c
@@ -1773,7 +1773,7 @@ STATIC_INLINE void gc_mark_obj8(jl_ptls_t ptls, char *obj8_parent, uint8_t *obj8
     jl_value_t *new_obj;
     for (; obj8_begin < obj8_end; obj8_begin++) {
         jl_prefetch(obj8_parent + *obj8_begin + 4);
-	new_obj = ((jl_value_t **)obj8_parent)[*obj8_begin];
+        new_obj = ((jl_value_t **)obj8_parent)[*obj8_begin];
         if (new_obj)
             verify_parent2("object", obj8_parent, &new_obj, "field(%d)",
                            gc_slot_to_fieldidx(obj8_parent, &new_obj));
@@ -1791,7 +1791,7 @@ STATIC_INLINE void gc_mark_obj16(jl_ptls_t ptls, char *obj16_parent, uint16_t *o
     jl_value_t *new_obj;
     for (; obj16_begin < obj16_end; obj16_begin++) {
         jl_prefetch(obj16_parent + *obj16_begin + 4);
-	    new_obj = ((jl_value_t **)obj16_parent)[*obj16_begin];
+        new_obj = ((jl_value_t **)obj16_parent)[*obj16_begin];
         if (new_obj)
             verify_parent2("object", obj16_parent, &new_obj, "field(%d)",
                            gc_slot_to_fieldidx(obj16_parent, &new_obj));
@@ -1809,7 +1809,7 @@ STATIC_INLINE void gc_mark_obj32(jl_ptls_t ptls, char *obj32_parent, uint32_t *o
     jl_value_t *new_obj;
     for (; obj32_begin < obj32_end; obj32_begin++) {
         jl_prefetch(obj32_parent + *obj32_begin + 4);
-	    new_obj = ((jl_value_t **)obj32_parent)[*obj32_begin];
+        new_obj = ((jl_value_t **)obj32_parent)[*obj32_begin];
         if (new_obj)
             verify_parent2("object", obj32_parent, &new_obj, "field(%d)",
                            gc_slot_to_fieldidx(obj32_parent, &new_obj));
@@ -1848,7 +1848,7 @@ STATIC_INLINE void gc_mark_array8(jl_ptls_t ptls, jl_value_t *ary8_parent,
     for (; ary8_begin < ary8_end; ary8_begin += elsize) {
         for (uint8_t *pindex = elem_begin; pindex < elem_end; pindex++) {
             jl_prefetch(ary8_begin + *pindex + 4);
-	        new_obj = ary8_begin[*pindex];
+            new_obj = ary8_begin[*pindex];
             if (new_obj)
                 verify_parent2("array", ary8_parent, &new_obj, "elem(%d)",
                                gc_slot_to_arrayidx(ary8_parent, ary8_begin));
@@ -1870,7 +1870,7 @@ STATIC_INLINE void gc_mark_array16(jl_ptls_t ptls, jl_value_t *ary16_parent,
     for (; ary16_begin < ary16_end; ary16_begin += elsize) {
         for (uint16_t *pindex = elem_begin; pindex < elem_end; pindex++) {
             jl_prefetch(ary16_begin + *pindex + 4);
-	        new_obj = ary16_begin[*pindex];
+            new_obj = ary16_begin[*pindex];
             if (new_obj)
                 verify_parent2("array", ary16_parent, &new_obj, "elem(%d)",
                                gc_slot_to_arrayidx(ary16_parent, ary16_begin));
