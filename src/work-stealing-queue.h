63e05d72c4207c7bf1a8ae256eb2f95d3092019a
Valentin Churavy <v.churavy@gmail.com>
2023-04-20 10:37:04 -0400

    Provide eltsz at callsite

--- src/work-stealing-queue.h
+++ src/work-stealing-queue.h
@@ -4,6 +4,7 @@
 #define WORK_STEALING_QUEUE_H
 
 #include "julia_atomics.h"
+#include "assert.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -39,58 +40,61 @@ typedef struct {
     _Atomic(ws_array_t *) array;
 } ws_queue_t;
 
-static inline ws_array_t *ws_queue_push(ws_queue_t *q, void *elt) JL_NOTSAFEPOINT
+static inline ws_array_t *ws_queue_push(ws_queue_t *q, void *elt, int32_t eltsz) JL_NOTSAFEPOINT
 {
     int64_t b = jl_atomic_load_relaxed(&q->bottom);
     int64_t t = jl_atomic_load_acquire(&q->top);
     ws_array_t *ary = jl_atomic_load_relaxed(&q->array);
+    assert(ary->eltsz == eltsz);
     ws_array_t *old_ary = NULL;
     if (__unlikely(b - t > ary->capacity - 1)) {
-        ws_array_t *new_ary = create_ws_array(2 * ary->capacity, ary->eltsz);
+        ws_array_t *new_ary = create_ws_array(2 * ary->capacity, eltsz);
         for (int i = 0; i < ary->capacity; i++)
-            memcpy(new_ary->buffer + ((t + i) % new_ary->capacity) * ary->eltsz,
-                    ary->buffer + ((t + i) % ary->capacity) * ary->eltsz, ary->eltsz);
+            memcpy(new_ary->buffer + ((t + i) % new_ary->capacity) * eltsz,
+                    ary->buffer + ((t + i) % ary->capacity) * eltsz, eltsz);
         jl_atomic_store_release(&q->array, new_ary);
         old_ary = ary;
         ary = new_ary;
     }
-    memcpy(ary->buffer + (b % ary->capacity) * ary->eltsz, elt, ary->eltsz);
+    memcpy(ary->buffer + (b % ary->capacity) * eltsz, elt, eltsz);
     jl_fence_release();
     jl_atomic_store_relaxed(&q->bottom, b + 1);
     return old_ary;
 }
 
-static inline void ws_queue_pop(ws_queue_t *q, void *dest) JL_NOTSAFEPOINT
+static inline void ws_queue_pop(ws_queue_t *q, void *dest, int32_t eltsz) JL_NOTSAFEPOINT
 {
     int64_t b = jl_atomic_load_relaxed(&q->bottom) - 1;
     ws_array_t *ary = jl_atomic_load_relaxed(&q->array);
+    assert(ary->eltsz == eltsz);
     jl_atomic_store_relaxed(&q->bottom, b);
     jl_fence();
     int64_t t = jl_atomic_load_relaxed(&q->top);
     if (__likely(t <= b)) {
-        memcpy(dest, ary->buffer + (b % ary->capacity) * ary->eltsz, ary->eltsz);
+        memcpy(dest, ary->buffer + (b % ary->capacity) * eltsz, eltsz);
         if (t == b) {
             if (!jl_atomic_cmpswap(&q->top, &t, t + 1))
-                memset(dest, 0, ary->eltsz);
+                memset(dest, 0, eltsz);
             jl_atomic_store_relaxed(&q->bottom, b + 1);
         }
     }
     else {
-        memset(dest, 0, ary->eltsz);
+        memset(dest, 0, eltsz);
         jl_atomic_store_relaxed(&q->bottom, b + 1);
     }
 }
 
-static inline void ws_queue_steal_from(ws_queue_t *q, void *dest) JL_NOTSAFEPOINT
+static inline void ws_queue_steal_from(ws_queue_t *q, void *dest, int32_t eltsz) JL_NOTSAFEPOINT
 {
     int64_t t = jl_atomic_load_acquire(&q->top);
     jl_fence();
     int64_t b = jl_atomic_load_acquire(&q->bottom);
     if (t < b) {
         ws_array_t *ary = jl_atomic_load_relaxed(&q->array);
-        memcpy(dest, ary->buffer + (t % ary->capacity) * ary->eltsz, ary->eltsz);
+        assert(ary->eltsz == eltsz);
+        memcpy(dest, ary->buffer + (t % ary->capacity) * eltsz, eltsz);
         if (!jl_atomic_cmpswap(&q->top, &t, t + 1))
-            memset(dest, 0, ary->eltsz);
+            memset(dest, 0, eltsz);
     }
 }
 
